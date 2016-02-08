5359ca6ea6d6073aecdb4d91c5410701c3ae81a6
luchanso <oleg@loutchansky.com>
2016-02-08 14:07:50 +0300

    Add skills template :fries:

---
+++ src/skills.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var skills = {};
+
+// node
+skills.node           = false;
+skills.express        = false;
+skills.socket         = false;
+skills.nodeStreams    = false;
+skills.nodeCluster    = false;
+skills.nodeCrypto     = false;
+skills.nodeHTTP       = false;
+skills.meteor         = false;
+
+// database
+skills.mysql          = false;
+skills.couchdb        = false;
+skills.leveldb        = false;
+skills.mongodb        = false;
+skills.neo4j          = false;
+skills.postgresql     = false;
+skills.redis          = false;
+skills.sqllite        = false;
+skills.elasticsearch  = false;
+
+// Front-end
+skills.jquery         = false;
+skills.angularjs      = false;
+skills.react          = false;
+skills.redux          = false;
+skills.backbone       = false;
+skills.todomvc        = false;
+skills.emberjs        = false;
+skills.mean           = false;
+skills.polymer        = false;
+skills.knockout       = false;
+skills.frameworkjs    = false;
+skills.spine          = false;
+skills.brick          = false;
+skills.nuclearjs      = false;
+skills.dojo           = false;
+skills.matreshka      = false;
+skills.knockout       = false;
+
+module.exports = skills;
